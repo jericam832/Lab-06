33368ffc029e44c77e50c7c054a2828f7d9152c8
jericam832 <jericam32@gmail.com>
2019-10-08 09:45:04 -0700

    meh. I changed some variables

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
 var parentEl = document.getElementById('parentElement');
 // parentEl.textContent = 'Salmon Cookie Sales';
 
-var child1 = document.createElement('h1');
-child1.textContent = 'We Sell Cookies!';
-parentEl.appendChild(child1);
+var title = document.createElement('h1');
+title.textContent = 'We Sell Cookies!';
+parentEl.appendChild(title);
+var totalForLocation = document.createElement('li');
 
 var seattleStand = {
   shopHours: ['6am', '7am', '8am','9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
@@ -18,30 +19,30 @@ var seattleStand = {
     for (var i = 0; i < this.shopHours.length; i++) {
       var randomNum =  Math.floor(Math.random() * Math.floor(this.maxCustomers - this.minCustomers + 1) + this.minCustomers);
       this.totalCustomers.push(randomNum);
-    } 
+    }
   },
   locationName: 'Seattle',
   render: function() {
     var totalPerDay = 0;
     for (var i = 0; i < this.shopHours.length; i++){
-      var childEl1 = document.createElement('li');
+      var seattle = document.createElement('li');
       var totalPerHour = Math.floor(this.totalCustomers[i] * this.avgCookieSale);
       totalPerDay += totalPerHour;
-      childEl1.textContent = `${this.shopHours[i]} ${totalPerHour} cookies`;
-      parentEl.appendChild(childEl1);
+      seattle.textContent = `${this.shopHours[i]} ${totalPerHour} cookies`;
+      parentEl.appendChild(seattle);
     }
     this.locationTotal = totalPerDay;
   }
-}
+};
 seattleStand.randomCustomers();
 var cityName = document.createElement('h2');
 parentEl.appendChild(cityName);
 cityName.textContent = seattleStand.locationName;
 seattleStand.render();
 // console.log(seattleStand.locationTotal);
-var totalForLocation = document.createElement('li');
+
 parentEl.appendChild(totalForLocation);
-totalForLocation.textContent = "Total: " + seattleStand.locationTotal + " cookies";
+totalForLocation.textContent = 'Total: ' + seattleStand.locationTotal + ' cookies';
 
 var tokyoStand = {
   shopHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
@@ -61,24 +62,24 @@ var tokyoStand = {
   render: function() {
     var totalPerDay = 0;
     for (var i = 0; i < this.shopHours.length; i++) {
-      var childEl2 = document.createElement('li');
+      var tokyo = document.createElement('li');
       var totalPerHour = Math.floor(this.totalCustomers[i] * this.avgCookieSale);
       totalPerDay += totalPerHour;
-      childEl2.textContent = `${this.shopHours[i]} ${totalPerHour} cookies`;
-      parentEl.appendChild(childEl2);
+      tokyo.textContent = `${this.shopHours[i]} ${totalPerHour} cookies`;
+      parentEl.appendChild(tokyo);
     }
     this.locationTotal = totalPerDay;
   }
-}
+};
 tokyoStand.randomCustomers();
 var cityName2 = document.createElement('h2');
 parentEl.appendChild(cityName2);
 cityName2.textContent = tokyoStand.locationName;
 tokyoStand.render();
 // console.log(tokyoStand.locationTotal);
-var totalForLocation = document.createElement('li');
+
 parentEl.appendChild(totalForLocation);
-totalForLocation.textContent = "Total: " + tokyoStand.locationTotal + " cookies";
+totalForLocation.textContent = 'Total: ' + tokyoStand.locationTotal + ' cookies';
 
 var dubaiStand = {
   shopHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
@@ -96,26 +97,26 @@ var dubaiStand = {
   },
   locationName: 'Dubai',
   render: function () {
-  var totalPerDay = 0;
+    var totalPerDay = 0;
     for (var i = 0; i < this.shopHours.length; i++) {
-      var childEl3 = document.createElement('li');
+      var dubai = document.createElement('li');
       var totalPerHour = Math.floor(this.totalCustomers[i] * this.avgCookieSale);
       totalPerDay += totalPerHour;
-      childEl3.textContent = `${this.shopHours[i]} ${totalPerHour} cookies`;
-      parentEl.appendChild(childEl3);
+      dubai.textContent = `${this.shopHours[i]} ${totalPerHour} cookies`;
+      parentEl.appendChild(dubai);
     }
     this.locationTotal = totalPerDay;
   }
-}
+};
 dubaiStand.randomCustomers();
 var cityName3 = document.createElement('h2');
 parentEl.appendChild(cityName3);
 cityName3.textContent = dubaiStand.locationName;
 dubaiStand.render();
 // console.log(dubaiStand.locationTotal);
-var totalForLocation = document.createElement('li');
+
 parentEl.appendChild(totalForLocation);
-totalForLocation.textContent = "Total: " + dubaiStand.locationTotal + " cookies";
+totalForLocation.textContent = 'Total: ' + dubaiStand.locationTotal + ' cookies';
 
 var parisStand = {
   shopHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
@@ -135,24 +136,24 @@ var parisStand = {
   render: function () {
     var totalPerDay = 0;
     for (var i = 0; i < this.shopHours.length; i++) {
-      var childEl4 = document.createElement('li');
+      var paris = document.createElement('li');
       var totalPerHour = Math.floor(this.totalCustomers[i] * this.avgCookieSale);
       totalPerDay += totalPerHour;
-      childEl4.textContent = `${this.shopHours[i]} ${totalPerHour} cookies`;
-      parentEl.appendChild(childEl4);
+      paris.textContent = `${this.shopHours[i]} ${totalPerHour} cookies`;
+      parentEl.appendChild(paris);
     }
     this.locationTotal = totalPerDay;
   }
-}
+};
 parisStand.randomCustomers();
 var cityName4 = document.createElement('h2');
 parentEl.appendChild(cityName4);
 cityName4.textContent = parisStand.locationName;
 parisStand.render();
 // console.log(parisStand.locationTotal);
-var totalForLocation = document.createElement('li');
+
 parentEl.appendChild(totalForLocation);
-totalForLocation.textContent = "Total: " + parisStand.locationTotal + " cookies";
+totalForLocation.textContent = 'Total: ' + parisStand.locationTotal + ' cookies';
 
 var limaStand = {
   shopHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
@@ -172,21 +173,21 @@ var limaStand = {
   render: function () {
     var totalPerDay = 0;
     for (var i = 0; i < this.shopHours.length; i++) {
-      var childEl5 = document.createElement('li');
+      var lima = document.createElement('li');
       var totalPerHour = Math.floor(this.totalCustomers[i] * this.avgCookieSale);
       totalPerDay += totalPerHour;
-      childEl5.textContent = `${this.shopHours[i]} ${totalPerHour} cookies`;
-      parentEl.appendChild(childEl5);
+      lima.textContent = `${this.shopHours[i]} ${totalPerHour} cookies`;
+      parentEl.appendChild(lima);
     }
     this.locationTotal = totalPerDay;
   }
-}
+};
 limaStand.randomCustomers();
 var cityName5 = document.createElement('h2');
 parentEl.appendChild(cityName5);
 cityName5.textContent = limaStand.locationName;
 limaStand.render();
 // console.log(limaStand.locationTotal);
-var totalForLocation = document.createElement('li');
+
 parentEl.appendChild(totalForLocation);
-totalForLocation.textContent = "Total: " + limaStand.locationTotal + " cookies";
+totalForLocation.textContent = 'Total: ' + limaStand.locationTotal + ' cookies';
